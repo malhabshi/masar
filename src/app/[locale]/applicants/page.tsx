c4f6d09c84fa503ddf9a4d0e1fd4583f84141bf6
Mohammed Alhabshi <malhabshi@q8sf.com>
2026-02-19 03:50:39 +0000

    404 error still her

--- src/app/[locale]/applicants/page.tsx
+++ src/app/[locale]/applicants/page.tsx
@@ -1,4 +1,2 @@
-
 // This file is intentionally left empty to resolve a build-time routing conflict.
 // The active page for this route is located at /src/app/[locale]/(app)/applicants/page.tsx.
-    
